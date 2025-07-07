3f012f2750931840c173919caeb282cb25581c13
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-07 10:06:25 +0200

    create restaurant class

---
+++ nivell-2_ex-1/src/Restaurant.java
@@ -0,0 +1,9 @@
+public class Restaurant {
+    private String name;
+    private int rating;
+
+    public Restaurant(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+}
