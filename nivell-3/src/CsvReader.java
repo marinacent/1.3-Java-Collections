d7a555781f3a1c088767f577acc35f967d268d1a
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-07 13:33:06 +0200

    feat: create CsvReader class

---
+++ nivell-3/src/CsvReader.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CsvReader {
+
+    public String[] read(String path, String delimiter) throws IOException {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line;
+            String[] values = null;
+            while ((line = reader.readLine()) != null) {
+                values = line.split(delimiter);
+            }
+            return values;
+
+    }
+
+
+}
