99f6ed5a867f55f2ff3bb5b3a0176cd603fb1dd3
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-07 13:39:54 +0200

    fix: CsvReader.read returns all lines, not just the last one

--- nivell-3/src/CsvReader.java
+++ nivell-3/src/CsvReader.java
@@ -1,17 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CsvReader {
 
-    public static String[] read(String path, String delimiter) throws IOException {
+    public static ArrayList<String[]> read(String path, String delimiter) throws IOException {
             BufferedReader reader = new BufferedReader(new FileReader(path));
             String line;
             String[] values = null;
+            ArrayList<String[]> listOfValues = new ArrayList<>();
             while ((line = reader.readLine()) != null) {
                 values = line.split(delimiter);
+                listOfValues.add(values);
             }
-            return values;
+            return listOfValues;
 
     }
 
